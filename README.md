203a5467b000b6ff242933e75c741d97f51dd7d4
satig <satigrv8477@gmail.com>
2024-03-14 23:55:45 +0530

    Create the readjson file and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Main
+# main
+
+This is used for CI/CD pipeline when a json file is inserted in S3 it triggered th lambda and using the
+AWS codebuild pipeline runs and lambda fuction take the json and convert it into dataframe
---
+++ readjson.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+import pandas
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    # Initialize S3 client
+    s3 = boto3.client('s3')
+    
+    
+    # Read JSON file from S3
+    response = s3.get_object(Bucket=bucket, Key=key)
+    json_data = response['Body'].read().decode('utf-8')
+        
+        # Convert JSON string to DataFrame
+    df = pd.read_json(json_data)
+        
+    return df
+
